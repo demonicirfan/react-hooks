769f0b2e8280e9bc036608a115eccf55e86dc3ab
demonicirfan <hashtag.irfan@gmail.com>
2021-08-17 23:59:21 +0530

    random color

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react'
-
+import React, { useState } from "react";
 const App = () => {
+  const [backgroundColor, setBackgroundColor] = useState("lightblue");
+  const getRandomColor = () => {
+    return "#" + Math.random().toString(16).slice(2, 8);
+  };
+  const changeBackgroundColor = () => {
+    setBackgroundColor(getRandomColor());
+  };
   return (
-    <div>
-      App
-    </div>
-  )
-}
+    <main>
+      <div style={{ backgroundColor: backgroundColor }} >
+      <button onClick={changeBackgroundColor}>Change Color</button>
+      </div>
+    </main>
+  );
+};
 
-export default App
+export default App;
